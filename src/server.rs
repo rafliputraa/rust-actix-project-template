9e163107c4765ff2c2a1978f598d92b970319be4
Rafli Putra <rafli.putra712@gmail.com>
2024-05-05 22:47:52 +0700

    Feature/add caching mechanism (#3)
    * init redis integration, update config, env for redis, utilize caching in fetch users. TODO: utilize caching for the other 2 endpoints
    
    * finished the caching mechanism impl
    
    ---------
    
    Co-authored-by: rafliputra <rafli.putra@mandirisek.co.id>

--- src/server.rs
+++ src/server.rs
@@ -3,17 +3,24 @@ use dotenv::dotenv;
 use env_logger::Builder;
 use sqlx::{Pool, Postgres};
 use crate::config::CONFIG;
-use crate::database::create_pool;
+use crate::database::{create_pool, create_redis_client};
 use crate::routes::routes;
 use std::io::Write;
+use std::sync::Arc;
 use log::{error, info};
+use redis::Client;
 
-pub struct AppState { pub db: Pool<Postgres>
+pub struct AppState {
+    pub db: Pool<Postgres>,
+    pub cache: Arc<Client>,
 }
 
 pub async fn server() -> std::io::Result<()> {
+
+    // Load the environment variables
     dotenv().ok();
 
+    // Build the log format
     Builder::from_env(env_logger::Env::default().default_filter_or(&CONFIG.log_level))
         .format(|buf, record| {
             writeln!(
@@ -26,11 +33,11 @@ pub async fn server() -> std::io::Result<()> {
         })
         .init();
 
+    // Initialize postgres pool connection
     let pool;
-
     match create_pool().await {
         Ok(conn) => {
-            pool = conn
+            pool = conn;
         }
         Err(err) => {
             error!("Failed to create database pool: {}", err);
@@ -38,12 +45,27 @@ pub async fn server() -> std::io::Result<()> {
         }
     }
 
+    // Initialize redis client connection
+    let redis_client;
+    match create_redis_client().await {
+        Ok(client) => {
+            redis_client = client;
+        }
+        Err(err) => {
+            error!("Failed to create redis connection: {}", err);
+            std::process::exit(1);
+        }
+    }
+    let redis_client_arc = Arc::new(redis_client);
+
     info!("🚀 Server started successfully");
+    // Start the server
     let server = HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .app_data(web::Data::new(AppState{
-                db: pool.clone()
+                db: pool.clone(),
+                cache: redis_client_arc.clone(),
             }))
             .configure(routes)
     });
--- src/services.rs
+++ src/services.rs
@@ -5,15 +5,16 @@ use crate::errors::ApiError;
 use crate::helpers::respond_json;
 use crate::server::AppState;
 use log::debug;
+use redis::AsyncCommands;
 
-#[derive(Serialize, FromRow, Debug)]
+#[derive(Serialize, Deserialize, FromRow, Debug)]
 pub struct User {
     id: i32,
     first_name: String,
     last_name: String,
 }
 
-#[derive(Serialize, FromRow, Debug)]
+#[derive(Serialize, Deserialize, FromRow, Debug)]
 pub struct Article {
     id: i32,
     title: String,
@@ -28,27 +29,55 @@ pub struct CreateArticleBody {
 }
 
 pub async fn fetch_users(state: Data<AppState>) -> Result<Json<Vec<User>>, ApiError>{
-    let users = sqlx::query_as::<_, User>("select id, first_name, last_name from users")
-        .fetch_all(&state.db)
-        .await?;
-    debug!("fetch_users - OUTPUT | Users: {:?}", users);
+    let users;
+    let mut redis_conn = state.cache.get_multiplexed_tokio_connection().await.expect("Failed to get the redis connection");
+    // Retrieve data from cache
+    let cached_data: Option<String> = redis_conn.get("data:users").await.expect("Failed to retrieve the cached data");
+    if let Some(data) = cached_data {
+        debug!("Cached Data: {:?}", data);
+        let parsed_data: Vec<User> = serde_json::from_str(&data).expect("Failed to deserialize the users data");
+        users = parsed_data;
+    } else {
+        debug!("No data found in cache.");
+        users = sqlx::query_as::<_, User>("select id, first_name, last_name from users")
+            .fetch_all(&state.db)
+            .await?;
+        debug!("fetch_users - OUTPUT | Users: {:?}", users);
+        let data_to_cache = serde_json::to_string(&users).expect("Failed to serialize the users data");
+        let _: () = redis_conn.set_ex("data:users", data_to_cache, 7200).await.expect("Failed to set the users data to redis");
+    }
+
     respond_json(users)
 }
 
 pub async fn fetch_user_articles(state: Data<AppState>, path: Path<i32>) -> Result<Json<Vec<Article>>, ApiError> {
     let id = path.into_inner();
+    let articles;
+    let mut redis_conn = state.cache.get_multiplexed_tokio_connection().await.expect("Failed to get redis connection");
 
-    debug!("fetch_user_articles - INPUT | id: {}", id);
-    let articles = sqlx::query_as::<_, Article> (
-        "select id, title, content, created_by from articles where created_by = $1"
-    )
-        .bind(id)
-        .fetch_all(&state.db)
-        .await?;
-    debug!("fetch_user_articles - OUTPUT | Articles: {:?}", articles);
-    if articles.is_empty() {
-        return Err(ApiError::NotFound);
+    let cached_data: Option<String> = redis_conn.get(format!("data:articles:{}", &id)).await.expect("Failed to retrieve the cached data");
+    if let Some(data) = cached_data {
+        debug!("Cached Data: {:?}", data);
+        let parsed_data: Vec<Article> = serde_json::from_str(&data).expect("Failed to deserialize the articles data");
+        articles = parsed_data;
+    } else {
+        debug!("No data found in cache.");
+        debug!("fetch_user_articles - INPUT | id: {}", id);
+        articles = sqlx::query_as::<_, Article> (
+            "select id, title, content, created_by from articles where created_by = $1"
+        )
+            .bind(id)
+            .fetch_all(&state.db)
+            .await?;
+
+        if articles.is_empty() {
+            return Err(ApiError::NotFound);
+        }
+        debug!("fetch_user_articles - OUTPUT | Articles: {:?}", articles);
+        let data_to_cache = serde_json::to_string(&articles).expect("Failed to serialize the users data");
+        let _: () = redis_conn.set_ex(format!("data:articles:{}", &id), data_to_cache, 7200).await.expect("Failed to set the articles data to redis");
     }
+
     respond_json(articles)
 }
 
@@ -58,6 +87,8 @@ pub async fn create_user_article(
     body: Json<CreateArticleBody>,
 ) -> Result<Json<Article>, ApiError> {
     let id = path.into_inner();
+    let mut redis_conn = state.cache.get_multiplexed_tokio_connection().await.expect("Failed to get redis connection");
+
     debug!("create_user_article - INPUT | Body: {:?}", body);
    let article = sqlx::query_as::<_, Article>(
         "INSERT INTO articles (title, content, created_by) VALUES ($1, $2, $3) RETURNING id, title, content, created_by",
@@ -68,5 +99,6 @@ pub async fn create_user_article(
         .fetch_one(&state.db)
         .await?;
     debug!("create_user_article - OUTPUT | Article: {:?}", article);
+    let _: () = redis_conn.del(format!("data:articles:{}", &id)).await.expect("Failed to delete the articles data from redis");
     respond_json(article)
 }
